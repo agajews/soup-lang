b4aeb4605e06006ece9a4874f2f604ffbdcaf8df
agajews <agajews@gmail.com>
2019-11-19 01:36:12 -0500

    added a simple builtin parser

--- Quilt/Builtins.hs
+++ Quilt/Builtins.hs
@@ -4,20 +4,23 @@ module Quilt.Builtins (
 
 import Quilt.Value
 import Quilt.Eval
+import Quilt.Parser
 
 import Control.Monad.Except
 
 builtins :: [(String, Value)]
-builtins = [("+", function intPlus),
-            ("-", function intMinus)]
+builtins = [("+", function $ intBinOp (+)),
+            ("-", function $ intBinOp (-)),
+            ("parse-str", function parseStr)]
 
 function :: ([Value] -> Eval Value) -> Value
 function f = PrimFunc $ \args -> mapM eval args >>= f
 
-intPlus :: [Value] -> Eval Value
-intPlus [IntVal x, IntVal y] = return $ IntVal (x + y)
-intPlus _ = throwError InvalidArguments
+intBinOp :: (Integer -> Integer -> Integer) -> [Value] -> Eval Value
+intBinOp op [IntVal x, IntVal y] = return $ IntVal (op x y)
+intBinOp _ _ = throwError InvalidArguments
 
-intMinus :: [Value] -> Eval Value
-intMinus [IntVal x, IntVal y] = return $ IntVal (x - y)
-intMinus _ = throwError InvalidArguments
+parseStr :: [Value] -> Eval Value
+parseStr [StringVal m, StringVal s, v] = eval $ FuncCall (parserToVal p) [StringVal s, v]
+    where p = parseString m >> return (StringVal m)
+parseStr _ = throwError InvalidArguments
